61bf2799b25412ba7e9004cc0080a3fd4b742d30
phertyameen <aminubabafatima8@gmail.com>
2023-10-21 17:50:56 -0700

    linked some pages added more pages and function

--- js/index.js
+++ js/index.js
@@ -1,37 +1,21 @@
-let countdown = document.querySelector('#clock_countdown')
-let resend_code = document.querySelector('#resend_code')
+// let countdown = document.querySelector('#clock_countdown')
+// let resend_code = document.querySelector('#resend_code')
 
 
-// function for setting time interval
-let initial_countdown = countdown.innerHTML
-resend_code.style.display = 'none'
+// // function for setting time interval
+// let initial_countdown = countdown.innerHTML
+// resend_code.style.display = 'none'
 
-setInterval(e => {
-    initial_countdown =  initial_countdown > 0? initial_countdown - 1 : 0
+// setInterval(e => {
+//     initial_countdown =  initial_countdown > 0? initial_countdown - 1 : 0
 
-	countdown.innerHTML = initial_countdown
-    console.log(initial_countdown)
+// 	countdown.innerHTML = initial_countdown
+//     console.log(initial_countdown)
 
-	// console.log(count_size)
+// 	// console.log(count_size)
 
-    initial_countdown <= 0 ? resend_code.style.display = 'block': resend_code.style.display = 'none'
+//     initial_countdown <= 0 ? resend_code.style.display = 'block': resend_code.style.display = 'none'
 
-}, 1000)
+// }, 1000)
 
 // resend_code.addEventListener('click', setInterval(e))
-
-// signup auth
-const axios = require('axios');
-const signUpData = {
-    username: 'exampleuser',
-    password: 'password123',
-    // Other registration data
-  };
-  
-  axios.post('https://u-and-doc.onrender.com/api/user/signup', signUpData)
-    .then(response => {
-      console.log('Sign-up successful:', response.data);
-    })
-    .catch(error => {
-      console.error('Sign-up failed:', error.response.data);
-    });
--- js/password_pop_ups.js
+++ js/password_pop_ups.js
@@ -8,19 +8,89 @@ let reset_pw_main = document.getElementById('reset_pw_main')
 // required.innerHTML = required
 // console.log(required)
 
-let sign_up_btn = document.querySelector('.sign_up_btn')
-// console.log(sign_up_btn)
+// sign_up_btn.addEventListener('click', (e) =>  {
+//     e.preventDefault()
+//     const redirectUrl = 'verification_code.html'
 
-sign_up_btn.addEventListener('click', (e) =>  {
-    e.preventDefault()
-    const redirectUrl = 'verification_code.html'
-
-    window.location.href = redirectUrl;
-})
+//     window.location.href = redirectUrl;
+// })
 
 // console.log(success_pw_reset)
 // reset_pw_btn.onclick(e => {
 //     success_pw_reset.style.display = 'block'
 //     reset_pw_main.style.display = 'none'
 // })
-    
+
+// user signup authentication
+
+// Get a reference to the form element
+const signupform = document.querySelector('#registrationForm');
+// console.log(form)
+
+signupform.addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent the default form submission
+
+    const form = e.target;
+    const formData = new FormData(form);
+
+    // Convert FormData to a JSON object
+    const data = {};
+    formData.forEach((value, key) => {
+        data[key] = value;
+    });
+
+    fetch("https://u-and-doc.onrender.com/api/user/signup", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    })
+        .then((response) => {
+            if (response.ok) {
+                // Handle a successful response here
+                console.log("User created successfully");
+
+                const successmessage = document.querySelector('.successmessage')
+                const loginbtn = document.querySelector('#loginbtn')
+                const sign_up_btn = document.querySelector('.sign_up_btn')
+                const footer = document.querySelector('.p_footer')
+
+                // sign_up_btn.addEventListener('click', (e) => {
+                    successmessage.style.display = 'block'
+                    signupform.style.display = 'none'
+                    footer.style.display = 'none'
+                // })
+
+                loginbtn.addEventListener('click', (e) => {
+                    window.location.href = "log_in.html"
+                })
+            } else {
+                // Handle errors here
+                console.error("Error creating user");
+            }
+        })
+        //   .then(data => {
+        //     if (data.userId) {
+        //         // Registration was successful, and the user ID is available
+        //         const userId = data.userId;
+
+        //         // Store the user ID (e.g., in a cookie or session storage) for future use
+        //         // Here's an example of how to set it in a session storage:
+        //         // sessionStorage.setItem("userId", userId);
+
+        //         // Redirect to the user profile page or perform other actions
+        //         // window.location.href = "./user_profile.html";
+
+        //     } else {
+        //         // Handle registration errors
+        //         console.error("Registration failed");
+
+        //     }
+        // })
+        .catch((error) => {
+            // Handle network errors here
+            console.error("Network error:", error);
+        });
+});
+
